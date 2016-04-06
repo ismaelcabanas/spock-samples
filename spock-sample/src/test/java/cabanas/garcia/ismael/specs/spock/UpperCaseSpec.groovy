9fa8228a3c8ab7796c56c176fb8ef2f70c471259
Ismael Cabañas Garcia <icabanas@ext.vectorsf.com>
2016-04-07 00:08:16 +0200

    Uso del bloque expect
    En este commit se muestra el uso del bloque expect.

--- spock-sample/src/test/java/cabanas/garcia/ismael/specs/spock/UpperCaseSpec.groovy
+++ spock-sample/src/test/java/cabanas/garcia/ismael/specs/spock/UpperCaseSpec.groovy
@@ -1,35 +1,37 @@
-package cabanas.garcia.ismael.specs.spock
+    package cabanas.garcia.ismael.specs.spock
 
-import cabanas.garcia.ismael.sample.spock.UpperCaseConverter
-import spock.lang.Specification
+    import cabanas.garcia.ismael.sample.spock.UpperCaseConverter
+    import spock.lang.Specification
 
-import java.nio.file.Paths
+    import java.nio.file.Paths
 
-/**
- * Created by Ismael on 04/04/2016.
- */
-class UpperCaseSpec extends Specification{
+    /**
+     * Created by Ismael on 04/04/2016.
+     */
+    class UpperCaseSpec extends Specification{
 
-    def UpperCaseConverter upperCaseConverter
+        def UpperCaseConverter upperCaseConverter
 
-    def "Convierte una lista de cadenas a mayúsculas"(){
+        def "Convierte una lista de cadenas a mayúsculas"(){
 
-        given: "Dada una lista con dos cadenas"
-        def str1 = "spock"
-        def str2 = "framework"
-        def list = new ArrayList<String>()
-        list.add(str1)
-        list.add(str2)
-        upperCaseConverter = new UpperCaseConverter()
+            given: "Dada una lista con dos cadenas"
+            def str1 = "spock"
+            def str2 = "framework"
+            def list = new ArrayList<String>()
+            list.add(str1)
+            list.add(str2)
+            upperCaseConverter = new UpperCaseConverter()
 
-        when: "Cuando aplicamos el UpperCaseConverter sobre la lista"
-        List<String> convertedList = upperCaseConverter.toUpperCase(list)
+            expect: "Debería devolver una lista de cadenas mayúsculas cuando aplicamos el UpperCaseConverter sobre una lista dada"
+            def convertedList = upperCaseConverter.toUpperCase(list)
+            convertedList.get(0) == "SPOCK"
+            convertedList.get(1) == "FRAMEWORK"
 
-        then: "Entonces todas las cadenas de la lista están en mayúsculas"
-        convertedList.get(0) == "SPOCK"
-        convertedList.get(1) == "FRAMEWORK"
+            and: "Debería devolver una lista vacía cuando aplicamos el UpperCaseConverter sobre una lista vacía"
+            def emptyConvertedList = upperCaseConverter.toUpperCase(new ArrayList<String>())
+            emptyConvertedList.size() == 0
+
+        }
+
 
     }
-
-
-}
