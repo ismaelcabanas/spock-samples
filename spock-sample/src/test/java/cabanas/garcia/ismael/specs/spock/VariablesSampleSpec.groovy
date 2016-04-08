a2cf0281e6d35921450aba67828a84e24a61f135
Ismael Cabañas Garcia <icabanas@ext.vectorsf.com>
2016-04-08 12:59:06 +0200

    Ejemplo de variables compartidas

---
+++ spock-sample/src/test/java/cabanas/garcia/ismael/specs/spock/VariablesSampleSpec.groovy
@@ -0,0 +1,45 @@
+package cabanas.garcia.ismael.specs.spock
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * Created by Ismael on 08/04/2016.
+ */
+class VariablesSampleSpec extends Specification{
+    def message = "Hola mundo!"
+
+    def obj1 = new Object()
+
+    @Shared
+    def obj2 = new Object()
+
+    def "first feature method"(){
+
+        println("Primer método de la feature")
+        println("Objeto obj1: " + obj1.toString())
+        println("Objeto obj2: " + obj2.toString())
+
+        when: "Mensaje se transforma en minúsculas"
+        message = message.toLowerCase()
+
+        then: "El mensaje se debería haber transformado en minúsculas"
+        message == "hola mundo!"
+
+    }
+
+    def "second feature method"(){
+
+        println("Segundo método de la feature")
+        println("Objeto obj1: " + obj1.toString())
+        println("Objeto obj2: " + obj2.toString())
+
+        when: "Mensaje se transforma en mayúsculas"
+        message = message.toUpperCase()
+
+        then: "El mensaje se debería haber transformado en mayúsculas"
+        message == "HOLA MUNDO!"
+
+    }
+
+}
